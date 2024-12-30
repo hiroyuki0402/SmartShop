42b17a9d0b3fb294e6b072a4d5221c4b36853fc3
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2024-12-30 16:19:18 +0900

    first commit

---
+++ SmartShop/SmartShop/SmartShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartShopApp.swift
+//  SmartShop
+//
+//  Created by SHIRAISHI HIROYUKI on 2024/12/30.
+//
+
+import SwiftUI
+
+@main
+struct SmartShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartShop/SmartShopTests/SmartShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartShopTests.swift
+//  SmartShopTests
+//
+//  Created by SHIRAISHI HIROYUKI on 2024/12/30.
+//
+
+import Testing
+@testable import SmartShop
+
+struct SmartShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
