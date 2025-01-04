cafc028d31a56a05928cde3e062f04f7ec22d35a
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2025-01-05 02:08:11 +0900

    [add] バックエンド側ログイン機能の追加
      - 変更点:
     	- ログイン機能の追加
    
       - 注意点
     	- なし
    
     ## 関連するIssue/チケット
     - なし

--- SmartShop-Server/routes/auth.js
+++ SmartShop-Server/routes/auth.js
@@ -8,6 +8,12 @@ const registerValidator = [
     body('password', 'パスワードを空にすることはできません!').not().isEmpty()
 ]
 
+const loginValidator = [
+    body('username', 'ユーザー名を空にすることはできません!').not().isEmpty(),
+    body('password', 'パスワードを空にすることはできません!').not().isEmpty()
+]
+
 routes.post('/register', registerValidator, authController.register)
+routes.post('/login', loginValidator, authController.login)
 
 module.exports = routes
